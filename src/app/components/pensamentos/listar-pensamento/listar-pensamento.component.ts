54e3c3d9c19cf499bdefa2306dcd0e0611d19224
Marco Pla <marco.pla@gmail.com>
2024-03-19 18:55:03 -0300

    feat ngClass added

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com o @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas earum impedit maxime officia perspiciatis consectetur nulla ea quas voluptatum perferendis, debitis architecto quisquam mollitia voluptatem natus corrupti cupiditate laboriosam labore?debitis architecto quisquam mollitia voluptatem natus corrupti cupiditate laboriosam labore?',
+      autoria: '',
+      modelo: 'modelo1',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
